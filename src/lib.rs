f8b481698d405e614602660847f31db247d3fec7
matt <mmatu21@gmail.com>
2016-12-21 22:14:19 -0500

    Added serde crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+#![feature(proc_macro)]
+#[macro_use]
+extern crate serde_derive;
 extern crate hyper;
 extern crate itertools;
 extern crate serde_json;
+extern crate serde;
 
 pub mod yahoo_finance;
